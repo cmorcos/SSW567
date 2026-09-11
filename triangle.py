6eaa8c0d6bacf139b146b8239aeccec568374cb3
cmorcos <morcoscharlotte@gmail.com>
2026-09-10 23:19:07 -0400

    triangle file and test

---
+++ triangle.py
@@ -0,0 +1,41 @@
+def classify_triangle(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return "none"
+ 
+    # triangle inequality
+    if a + b <= c or a + c <= b or b + c <= a:
+        return "none"
+ 
+    # classify triangle
+    triangle = None
+    if a == b == c:
+        triangle = "equilateral"
+    elif a == b or a == c or b == c:
+        triangle = "isosceles"
+    else:
+        triangle = "scalene"
+ 
+    # right angle check
+    sides = sorted([a, b, c])
+    x, y, z = sides[0], sides[1], sides[2]
+    if x**2 + y**2 == z**2:     # pythagoreom theorem for right angle
+        triangle += " right"
+    return triangle
+ 
+ 
+def main():
+    examples = [
+        (3, 3, 3),      # equilateral
+        (5, 5, 3),      # isosceles
+        (3, 4, 5),      # scalene right
+        (4, 5, 6),      # scalene
+        (1, 2, 10),     # none
+    ]
+
+    # formatting for clearer output
+    for a, b, c in examples:
+        print(f"classify_triangle({a}, {b}, {c}) = {classify_triangle(a, b, c)}")
+ 
+ 
+if __name__ == "__main__":
+    main()
